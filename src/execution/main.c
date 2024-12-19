024ae8a9570253fae290a044498d602cbcae7acd
Guadalupe Alvarez <gualvare@car15s3.42barcelona.com>
2024-12-19 15:07:07 +0100

    [EXECUTOR FIRST VERION WORKING]

--- src/execution/main.c
+++ src/execution/main.c
@@ -161,7 +161,7 @@ int main(int argc, char **argv, char **env)
             {
                 preprocess_tokens(&tkn_lst);
                 t_command **cmd_list = commands(tkn_lst);
-                execute_cmd(&cmd_list, env_lst);
+                execute_cmd(cmd_list, env_lst);
                 print_commands(line, cmd_list);
                 print_tokens(line, tkn_lst);
                 free_cmd_list(cmd_list);
--- test.c
+++ test.c
@@ -1,9 +1,20 @@
 #include "minishell.h"
+
+
+char *find(t_command *cmd)
+{
+
+}
 int main()
 {
-    char *str = "ls -l";
+    char *str = "ls";
+    char *args[3] = {" ", "-l", "a"};
     
     int i = 0;
+    char *new_str = ft_strjoin(str, args[0]);
+    printf("%s\n", new_str);
+    char *new_str2 = ft_strjoin(new_str, args[1]);
+     printf("%s\n", new_str2);
     char **res = ft_split(str, ' ');
     while (res[i])
     {
