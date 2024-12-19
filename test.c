d18d1be44850e505994d21a14874285c31bba081
Guadalupe Alvarez <gualvare@car15s3.42barcelona.com>
2024-12-19 18:34:27 +0100

    [BUILTIN WIP]

--- test.c
+++ test.c
@@ -1,26 +1,15 @@
 #include "minishell.h"
 
-
-char *find(t_command *cmd)
+int is_valid_key(char c)
 {
-
+    return (ft_isalpha(c) || c == '_');
 }
-int main()
-{
-    char *str = "ls";
-    char *args[3] = {" ", "-l", "a"};
-    
-    int i = 0;
-    char *new_str = ft_strjoin(str, args[0]);
-    printf("%s\n", new_str);
-    char *new_str2 = ft_strjoin(new_str, args[1]);
-     printf("%s\n", new_str2);
-    char **res = ft_split(str, ' ');
-    while (res[i])
-    {
-        printf("%s\n", res[i]);
-        i++;
-    }
 
+int main(int argc, char **argv)
+{
+    if (argc == 2)
+    {
+        printf("%i\n", is_valid_key(argv[1][0]));
+    }
 
 }
