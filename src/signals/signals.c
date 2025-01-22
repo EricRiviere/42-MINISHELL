3188e1ec03a8080489eb4e20a539921959ecbdd5
Guadalupe Alvarez <gualvare@cbr5s6.42barcelona.com>
2025-01-22 18:01:56 +0100

    [MINISHELL COMPLETAMENTE FUNCIONAL]

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -1,7 +1,5 @@
 #include "minishell.h"
 
-
-
 void	handle_signaled(int *status, int signal)
 {
 	if (signal == 2)
@@ -11,18 +9,11 @@ void	handle_signaled(int *status, int signal)
 	*status = 128 + signal;
 }
 
-
 void	parent_signals(void)
 {
 	signal(SIGINT, output_signals);
 	signal(SIGQUIT, SIG_IGN);
 }
-
-// void	child_signals(void)
-// {
-// 	signal(SIGINT, SIG_DFL);
-// 	signal(SIGQUIT, output_signals);
-// }
 
 void	here_signals(int sig)
 {
@@ -31,37 +22,16 @@ void	here_signals(int sig)
 		printf(">\n");
 		exit(130);
 	}
-    // signal(SIGQUIT, SIG_IGN);
 }
-
-// void	sigquit_signals(int sig)
-// {
-// 	(void)sig;
-// 	rl_on_new_line();
-// 	write(STDOUT_FILENO, "hola", 4);
-// 	rl_replace_line("", 0);
-// 	rl_redisplay();
-// }
-
-		// ft_printf("\n");
-		// rl_replace_line("", 0);
-		// rl_on_new_line();
-		// rl_redisplay();
 
 void	output_signals(int sig)
 {
-	
 	if (sig == SIGINT)
 	{
 		write(STDOUT_FILENO, "\n", 1);
 		rl_replace_line("", 0);
 		rl_on_new_line();
 		rl_redisplay();
+		get_status(1, 130);
 	}
-	// else if (sig == SIGQUIT)
-	// {
-	// 	write(STDOUT_FILENO, "Quit: core dump\n", 16);
-	// 	// rl_replace_line("", 0);
-	// }
-	//rl_redisplay();  // Redibujar el prompt
 }
